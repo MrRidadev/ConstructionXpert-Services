aeaad2011507d86cec358ade652d7e1d2a8332d5
ridaDev <kiratterida@gmail.com>
2025-03-24 09:50:16 +0000

    modification de sql

--- src/Sql/Construction.sql
+++ src/Sql/Construction.sql
@@ -18,9 +18,16 @@ create table taches (
                         date_debut date,
                         date_fin date,
                         ressource varchar(255),
-                        foreign key (id_projet) references projets(id_projet)
+                        foreign key (id_projet) references projets(id_projet) on delete cascade
 );
 
+INSERT INTO taches (id_projet, nom_tache, description, date_debut, date_fin, ressource)
+VALUES
+    (21, 'Étude de sol', 'Analyser la qualité du sol avant construction', '2024-04-01', '2024-04-05', 'Ahmed El Idrissi');
+INSERT INTO taches (id_projet, nom_tache, description, date_debut, date_fin, ressource)
+VALUES (21, 'Fondations', 'Construction des fondations en béton armé', '2024-04-06', '2024-04-15', 'Fatima Zahra'),
+       (18, 'Installation échafaudages', 'Mettre en place les échafaudages pour la façade', '2024-05-01', '2024-05-03', 'Youssef Amrani'),
+       (21, 'Peinture intérieure', 'Peindre les murs et plafonds', '2024-05-04', '2024-05-10', 'Salma Benjelloun');
 create table Ressources (
                             id_res int primary key auto_increment,
                             id_tache int,
@@ -29,3 +36,4 @@ create table Ressources (
                             quantite int,
                             fornisour varchar(255)
 );
+
